54868e952ad9d33f7778999f4e91a1ad1d4ccf21
Kaveesha Dihansi <cst22038@std.uwu.ac.lk>
2024-06-18 23:38:32 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        System.out.println("The addition is :" +(a+b));
+        System.out.println("The subtraction is :" +(a-b));
+        System.out.println("The multiplication  is :" +(a*b));
+        System.out.println("The division is :" +(a/b));
+        System.out.println("The reminder is :" +(a%b));
+    }
+}
---
+++ ConditionalOperator.java
@@ -0,0 +1,11 @@
+public class ConditionalOperator {
+    public static void main(String[] args) {
+
+        int num1= 3, num2 = 4, largest;
+
+        largest = (num1>num2)?num1:num2;
+
+        System.out.println("The largest no is :" +largest);
+
+    }
+}
